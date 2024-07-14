3892f6159b738750612e3ecb482ce48286e54e5e
Khalida <xalide.haqverdiyeva@gmail.com>
2024-07-14 17:05:25 +0400

    feat: added a modal for post method

--- src/js/blog.js
+++ src/js/blog.js
@@ -3,14 +3,14 @@ const BASE_URL = 'http://localhost:3000';
 
 //  FETCH DATA FROM API
 
-async function fetchData(endpoint) {
+async function fetchData(endpoint, cb) {
 
     const response = await fetch(`http://localhost:3000/${endpoint}`);
     const data = await response.json();
     localStorage.setItem('apiData', JSON.stringify(data));
-    addDataUI(data);
+    cb(data);
 }
-fetchData('article');
+fetchData('article', addDataUI);
 
 function addDataUI(data) {
     data.map(item => {
@@ -58,3 +58,35 @@ function addDataUI(data) {
     })
 
 }
+
+
+// OPEN AND CLOSE THE MODAL FOR POST
+
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".openModal")
+const closer = document.querySelector(".close");
+const addArticle = document.querySelector('.addArticle');
+
+closer.addEventListener("click", () => {
+    modal.style.display = "none"
+
+});
+
+addArticle.addEventListener("click", () => {
+    modal.style.display = "none"
+
+});
+
+
+btn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+
+// POST NEW DATA
+
