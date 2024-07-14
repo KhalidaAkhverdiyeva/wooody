6f21fc54cb1de34dcdf89e1182dd1ab2904e2866
Khalida <xalide.haqverdiyeva@gmail.com>
2024-07-14 13:27:42 +0400

    chore: setup custom API with db.json, install JSON Server and npm

--- src/js/home.js
+++ src/js/home.js
@@ -1,38 +1,38 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     new Swiper(".mySwiper", {
-//         spaceBetween: 0,
-//         centeredSlides: true,
-//         loop: true,
-//         autoplay: {
-//             delay: 5500,
-//             disableOnInteraction: true,
-//         },
-//         speed: 800,
+document.addEventListener('DOMContentLoaded', function () {
+    new Swiper(".mySwiper", {
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 5500,
+            disableOnInteraction: true,
+        },
+        speed: 800,
 
-//     });
-// });
+    });
+});
 
-// var swiperImages = document.querySelectorAll('.mySwiper .swiper-slide img');
-// swiperImages.forEach(function (img, index) {
-//     img.addEventListener('click', function () {
+var swiperImages = document.querySelectorAll('.mySwiper .swiper-slide img');
+swiperImages.forEach(function (img, index) {
+    img.addEventListener('click', function () {
 
-//         swiper.slideTo(index + 1);
-//     });
-// });
+        swiper.slideTo(index + 1);
+    });
+});
 
-// window.addEventListener('scroll', function () {
-//     let scrollToTop = document.getElementById('scrollToTop');
+window.addEventListener('scroll', function () {
+    let scrollToTop = document.getElementById('scrollToTop');
+    let header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        scrollToTop.classList.add('show-scroll-to-top');
+    } else {
+        scrollToTop.classList.remove('show-scroll-to-top');
+    }
+});
 
-//     if (window.scrollY > 50) {
-//         scrollToTop.classList.add('show-scroll-to-top');
-//     } else {
-//         scrollToTop.classList.remove('show-scroll-to-top');
-//     }
-// });
-
-// document.getElementById('scrollToTop').addEventListener('click', function () {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: 'smooth'
-//     });
-// });
+document.getElementById('scrollToTop').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
